1307304da871d4ee9cd1793e56ccf8ad5575cdcc
Eliza Ahmed <elizarafee@gmail.com>
2020-05-15 20:04:16 +0600

    variables, constants and literals

---
+++ varConstLit.js
@@ -0,0 +1,8 @@
+// Constants can't be changed. The value we directly assign to any variable is called literals.
+// Variable and constants are called indentifier.
+
+var bride = 'Eliza Ahmed ';
+const GROOM = 'Hasan Tareque ';
+let firstname = "Hasan";
+console.log(bride, GROOM);
+console.log(firstname);
