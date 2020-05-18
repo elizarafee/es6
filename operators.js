48e624f0e67d0c004ef892c8dc16879c9f4d8141
Eliza Ahmed <elizarafee@gmail.com>
2020-05-19 00:00:10 +0600

    Operators

---
+++ operators.js
@@ -0,0 +1,17 @@
+// Arithmetic operator : +, -, *, /, ++, --
+// Comparison operator : Strict equality (===, !==), obstract equality (== , !=), relational
+// == checks the equaity of value, === checks the equality of datatype
+// Logical operator : &&, ||, !
+// Assignment operator : additional(+=), subsractional)(-=).....(*=)(/=), left shift(<<=), right shift(>>=), right shift zero fill(>>>>=), and(&=), or(|=), xor(^=)
+
+let a = 10, b = 10, c = 'eliza', d = 'tareque';
+if(a==b){
+    console.log('value is the same');
+}else{
+    console.log('value is not the same');
+}
+if(a===c){
+    console.log('datatype is the same');
+}else{
+    console.log('datatype is not the same')
+}
