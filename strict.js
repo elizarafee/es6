4d291507ab6ba3e578d0cab1ecb00ed8cb7d9e66
Eliza Ahmed <elizarafee@gmail.com>
2020-05-23 21:49:09 +0600

    Strict

---
+++ strict.js
@@ -0,0 +1,7 @@
+'use strict'
+
+
+ss = 30;
+console.log(ss);
+
+// without the 'use strict' on the first line the ss would be printed coz the that time the JS file would take the SS as a global variable
