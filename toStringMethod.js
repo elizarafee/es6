381c4f6b8e3ddfacd811eb9e3fac39c27b47b25d
Eliza Ahmed <elizarafee@gmail.com>
2020-05-27 21:16:45 +0600

    toString() method

---
+++ toStringMethod.js
@@ -0,0 +1,6 @@
+let a = 12345;
+console.log(a);
+console.log(a.toString());
+// the outputs are same but the data type is differrent
+console.log(typeof a);                  // number
+console.log(typeof a.toString());       // string
