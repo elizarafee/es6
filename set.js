4186d6bc9917aabe5e97800b92f8c073ca13ca1c
Eliza Ahmed <elizarafee@gmail.com>
2020-05-28 20:31:43 +0600

    set() : constructor

---
+++ set.js
@@ -0,0 +1,4 @@
+// set() object is identical to map object but set object allows you to store unique values of any type. Where duplicates are not allowed
+
+// creating constructor of set()
+const SET = new Set();
