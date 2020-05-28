3293ad5aabf6b6ff185ed49abaa167f730e2008a
Eliza Ahmed <elizarafee@gmail.com>
2020-05-28 20:13:40 +0600

    WeakMap() : constructor

--- weakMap.js
+++ weakMap.js
@@ -7,3 +7,13 @@ let user = {
 console.log(user.name);
 user = null;
 console.log(user.name);      // now JS will put junk data to name
+
+// in case of weakMap() the keys have to be object
+// weakmap can't be iterated or cleared 
+// keys in weakmap can be garbage collected
+// garbage collection in js oerforms automatically & it's invisible to us
+// the garbage collection take care of memory management in JS
+// weakMap()'s property is PRIVATE
+
+// weakMap():
+let weakmap = new WeakMap();
