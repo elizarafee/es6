abdee22ffb7ae9d16eac34ce76f66ba0367297c7
Eliza Ahmed <elizarafee@gmail.com>
2020-05-28 20:34:04 +0600

    set() : add() : adding data

--- set.js
+++ set.js
@@ -2,3 +2,10 @@
 
 // creating constructor of set()
 const SET = new Set();
+
+// Add element in the set object using add() method;
+SET.add(1);
+SET.add({a : 'Hasan'});
+SET.add(function(){return 12;});
+SET.add([1, 3, 5.7]);
+console.log(SET);
